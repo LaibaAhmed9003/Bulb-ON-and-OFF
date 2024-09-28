52e308fbb72d77dfc6b57b4103ed4ac15627c8fc
LaibaAhmed9003 <laibaahmed9003@gmail.com>
2024-09-28 03:00:33 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# Bulb-ON-and-OFF" 
+"# Bulb-ON-and-OFF" 
---
+++ app.js
@@ -0,0 +1,8 @@
+var bulb = document.getElementById("bulb");
+
+function turnon(){
+    bulb.src="on.jpeg";
+}
+function turnoff(){
+    bulb.src="off.jpeg";
+}
